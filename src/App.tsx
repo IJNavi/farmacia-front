79421c0bb862f6feeeb6152d4d1c5f412c79c34c
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-01-27 13:59:52 -0300

    CardCategorias implementado.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import './App.css'
+import ListaCategorias from './components/categorias/listacategorias/ListaCategorias'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
             </Routes>
           </div>
           <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar() {
 
                     <div className='flex gap-4'>
                         <button className="px-4 py-2 rounded-md bg-blue-100 text-blue-600 hover:bg-blue-300 hover:text-white">Produtos</button>
-                        <button className="px-4 py-2 rounded-md bg-blue-100 text-blue-600 hover:bg-blue-300 hover:text-white">Categorias</button>
+                        <Link to='/categorias' className="px-4 py-2 rounded-md bg-blue-100 text-blue-600 hover:bg-blue-300 hover:text-white">Categorias</Link>
                         <button className="px-4 py-2 rounded-md bg-blue-100 text-blue-600 hover:bg-blue-300 hover:text-white">Cadastrar Categorias</button>
                     </div>
                 </div>
