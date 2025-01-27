fdbf01d47433703f3f6d06718fd98d735d7b2db9
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-01-27 11:56:37 -0300

    Desenvolvimento das páginas em andamento.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,31 +1,17 @@
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+            <div className="w-screen flex justify-center">
                 <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
+                    <div className="max-w-7xl flex flex-col items-center">
                         <h2>Seja Bem Vinde!</h2>
-                        <p>Aqui vendemos drogas, licitas.</p>
+                        <p>Aqui vendemos drogas, lícitas!</p>
                     </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <img 
-                            src="https://img.freepik.com/vetores-gratis/colecao-ingenua-de-adesivos-de-farmacia_23-2150804508.jpg" 
-                            alt="Imagem da Página Home" 
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://img.freepik.com/vetores-gratis/colecao-ingenua-de-adesivos-de-farmacia_23-2150804508.jpg"
+                            alt="Imagem da Página Home"
                             width="400px"
                         />
                     </div>
