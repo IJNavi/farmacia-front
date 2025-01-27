964c5643f4b288e34453156ede90b3e3823a8503
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-01-27 11:12:24 -0300

    PAGES: Dependencias instaladas.

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,38 @@
+function Home() {
+    return (
+        <>
+            <div style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <div>
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Aqui vendemos drogas, licitas.</p>
+                    </div>
+
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <img 
+                            src="https://img.freepik.com/vetores-gratis/colecao-ingenua-de-adesivos-de-farmacia_23-2150804508.jpg" 
+                            alt="Imagem da Página Home" 
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
